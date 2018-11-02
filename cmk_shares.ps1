529b9ba42ffbe4ba91b6e0a4780e7d84b184ecbb
Tipsmark <rt@steait.net>
2018-11-01 22:36:20 -0400

    Update cmk_shares.ps1

--- cmk_shares.ps1
+++ cmk_shares.ps1
@@ -1,2 +1,2 @@
 Foreach ($Share in (Get-WmiObject -Class Win32_Share | Select Name, Path))
-	{ Write-Host 0 Share.$(($Share.Name).Replace(" ","_")) - $($Share.Path) shared as $($Share.Name)}
+{ Write-Host 0 Share.$(($Share.Name).Replace(" ","_")) - $($Share.Path) shared as $($Share.Name) }
